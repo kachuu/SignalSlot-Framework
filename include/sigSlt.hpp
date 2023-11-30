9883cbf178f32911935b90350ab6f5741aa9de54
kachuu <kachuu@live.jp>
2023-11-30 14:34:11 +1100

    Update sigSlt.hpp

--- include/sigSlt.hpp
+++ include/sigSlt.hpp
@@ -4,51 +4,51 @@
 
 using namespace std;
 
-template <class... Args>
+template <class... TArgs>
 class TSlot
 {
 public:
     virtual ~TSlot() {};
-    virtual void slot(Args... args) = 0;
+    virtual void slot(TArgs... args) = 0;
 };
 
-template <class TObject, class... Args>
-class TReceiverSlot : public TSlot<Args...>
+template <class TObject, class... TArgs>
+class TReceiverSlot : public TSlot<TArgs...>
 {
 public:
-    TReceiverSlot(TObject *obj, void (TObject::*fun)(Args...)) : obj(obj), fun(fun) {}
-    void slot(Args... args) {
+    TReceiverSlot(TObject *obj, void (TObject::*fun)(TArgs...)) : obj(obj), fun(fun) {}
+    void slot(TArgs... args) {
         (obj->*fun)(args...);
     }
     TObject *obj;
-    void (TObject::*fun)(Args...);
+    void (TObject::*fun)(TArgs...);
 };
 
-template <class... Args>
+template <class... TArgs>
 class TSignal
 {
 public:
     template<class TObject>
-    void addSlot(TObject *obj, void (TObject::*fun)(Args...)) {
-        m_slots.push_back(make_shared<TReceiverSlot<TObject, Args...>>(obj, fun));
+    void addSlot(TObject *obj, void (TObject::*fun)(TArgs...)) {
+        m_slots.push_back(make_shared<TReceiverSlot<TObject, TArgs...>>(obj, fun));
     }
     template<class TObject>
     void removeSlot(TObject *obj) {
         for (auto it = m_slots.begin(); it != m_slots.end();) {
-            if (obj != ((TReceiverSlot<TObject, Args...>*)&*(*it))->obj) ++it;
+            if (obj != ((TReceiverSlot<TObject, TArgs...>*)&*(*it))->obj) ++it;
             else it = m_slots.erase(it);
         }
     }
     void removeSlot(void *obj) {
         m_slots.clear();
     }
-    void operator()(Args... args) {
-        for (shared_ptr<TSlot<Args...>> &obj : m_slots) {
+    void operator()(TArgs... args) {
+        for (shared_ptr<TSlot<TArgs...>> &obj : m_slots) {
             obj->slot(args...);
         }
     }
 private:
-    list<shared_ptr<TSlot<Args...>>> m_slots;
+    list<shared_ptr<TSlot<TArgs...>>> m_slots;
 };
 
 #define connect(sender, signal, receiver, slot) (sender)->signal.addSlot(receiver, &slot)
